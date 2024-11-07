ef259fc9c9254afc3bd6368a51e04598a9102fa1
TheRev <cesarhumbertogarciafelix@gmail.com>
2024-11-07 12:03:47 -0700

    parte 3

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using Librerias_UrielOsuna.Data.Services;
+using Librerias_UrielOsuna.Data.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+
+namespace Librerias_UrielOsuna.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BooksController : ControllerBase
+	{
+		public BoocksService _boocksService;
+
+		public BooksController(BoocksService boocksService)
+		{
+			_boocksService = boocksService;
+		}
+		[HttpPost("add-book")]
+		public IActionResult AddBook([FromBody]BookVM book)
+		{
+			_boocksService.AddBook(book);
+			return Ok();
+		}
+	}
+}
---
+++ Data/Services/BoocksService.cs
@@ -0,0 +1,37 @@
+﻿using Librerias_JUOO.Data;
+using Librerias_JUOO.Data.Models;
+using Librerias_UrielOsuna.Data.ViewModels;
+using System;
+using System.Threading;
+
+namespace Librerias_UrielOsuna.Data.Services
+{
+    public class BoocksService
+    {
+        private AppDbContext _context;
+        public BoocksService(AppDbContext context)
+        {
+            _context = context;
+
+        }
+        public void AddBook(BookVM book)
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Descripcion = book.Descripcion,
+                IsRead = book.IsRead,
+                DataRead = book.DataRead,
+                Rate = book.Rate,
+                Genero = book.Genero,
+                Autor = book.Autor,
+                CoverUrl = book.CoverUrl,
+                DateAdded = DateTime.Now,
+
+            };
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+
+	}
+}
